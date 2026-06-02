ab6f2e519084968493be4a93cc69c326b20c4f52
MissRaAradhana <aradhanamisra2005@gmail.com>
2026-06-02 13:35:33 +0530

    added file

---
+++ name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    char name [] = "Aradhana";
+    printf("Hello, %s!\n", name);
+    return 0;
+}
--- test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
- printf("ishigami senku");
-  return 0;
-}
